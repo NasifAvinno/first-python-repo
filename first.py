66e71365e80a6d2007bf5504bb518bef79fa9654
NasifAvinno <nasiferfan@gmail.com>
2026-04-11 13:30:49 +0600

    First commit

---
+++ first.py
@@ -0,0 +1,44 @@
+print("fuck you")
+print("BITCH")
+print("69+96")
+print(69+96)
+name = "avinno"
+age = 23
+gender = "sex" 
+print(" Name = ", name)
+print(" age = ", age)
+print(" gender = ", gender)
+a = 2
+b =3
+sum = a + b
+print("Sum = ", sum)
+
+name = input("Name = ")
+print(name)
+age = int(input("Age = "))
+print(age)
+price = float(input("price = "))
+print(price)
+
+
+
+if(age >= 18):
+    print("Voter")
+elif(age < 18):
+    print("non voter")
+else:
+    print("jonmo hoini")
+
+marks = int(input("Marks = "))
+if marks >= 90:
+    print("A")
+elif marks >= 80 and marks <= 89:
+    print("B")
+elif marks >= 70 and marks <= 79:
+    print("C")
+elif marks >= 60 and marks <= 69:
+    print("D")
+elif marks < 60:
+    print("F")
+else:
+    print("Absent")
